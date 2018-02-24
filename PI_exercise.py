ece2a229c748c78f66f9e79fe335773685e073c5
Eric Reyes <eric.reyes@datarobot.com>
2018-02-24 16:08:24 -0500

    Set instructions

---
+++ PI_exercise.py
@@ -0,0 +1,3 @@
+# To find out if a user is in a certain city, we have to see if they are between IP1 and IP2
+# Implement a data structure that can hold 2 IPs and a city. IP format:  255.255.255.255
+
